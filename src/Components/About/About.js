2eccc00332b1ecab21859fdd0dc69c681ac48f08
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-03-25 02:16:47 +0700

    commit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,21 +3,121 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div class="slider_area">
-                <div class="single_slider  d-flex align-items-center slider_bg_1 overlay">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-lg-10 col-md-10">
-                                <div class="slider_text">
-                                    <h3 class="display-1">
-                                        This is about page
-                                    </h3>
+            <>
+                <section className="bg-light page-section" id="team">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12 text-center">
+                                <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+                                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-3">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/21192103_1297132797080400_4031736074795521275_n.jpg?_nc_cat=108&_nc_sid=7aed08&_nc_ohc=02bVUxCVt80AX9AqXbY&_nc_ht=scontent-hkt1-1.xx&oh=dd97c9c4841f13660eb23854ad417a8f&oe=5EA15954" alt="" />
+                                    <h4>Tieu Trung Hoa</h4>
+                                    <p className="text-muted">Full-stack Developer</p>
+                                    <ul className="list-inline social-buttons">
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/86277496_1089872311351372_7538710456899207168_n.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=obcSo9vjtGsAX_qjkbY&_nc_ht=scontent-hkt1-1.xx&oh=c34bd4f7202064c19ff131d697466397&oe=5EA0B821" alt="" />
+                                    <h4>Pham Thi Hanh</h4>
+                                    <p className="text-muted text-capitalize">Back-end Supporter</p>
+                                    <ul className="list-inline social-buttons">
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-9/88069398_801012263728189_7772348040622374912_n.jpg?_nc_cat=106&_nc_sid=85a577&_nc_ohc=vm_VEGtJhHcAX_y37oL&_nc_ht=scontent-hkt1-1.xx&oh=7986c71f6df86f7b7ea02eac80a625cc&oe=5EA121F0" alt="" />
+                                    <h4>Vu Minh Duc</h4>
+                                    <p className="text-muted">Content Creator</p>
+                                    <ul className="list-inline social-buttons">
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-sm-3">
+                                <div className="team-member">
+                                    <img className="mx-auto rounded-circle" src="https://scontent-hkt1-1.xx.fbcdn.net/v/t31.0-8/p960x960/19222575_142172749667863_7033633463980221645_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=mA7VPGHmcqEAX_u9RNg&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=e4ace821e7d2f0809cd88d8b3f5f4993&oe=5EA15DED" alt="" />
+                                    <h4>Mai Minh Hieu</h4>
+                                    <p className="text-muted">Front-end Supporter</p>
+                                    <ul className="list-inline social-buttons">
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </a>
+                                        </li>
+                                        <li className="list-inline-item">
+                                            <a href="/">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
+                        <div className="row">
+                            <div className="col-lg-8 mx-auto text-center">
+                                <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </section>
+            </>
         )
     }
 }
